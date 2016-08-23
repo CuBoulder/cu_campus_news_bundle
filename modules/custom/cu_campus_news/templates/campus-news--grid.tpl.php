37a7d335d026758be7f15b2cb83ffaf7ff7456b1
kevincrafts <kevin.crafts@colorado.edu>
2016-08-23 14:31:50 -0600

    adding modules

---
+++ modules/custom/cu_campus_news/templates/campus-news--grid.tpl.php
@@ -0,0 +1,7 @@
+<div class="article-view-mode-grid col-lg-4 col-md-4 col-sm-6 col-xs-12">
+  <?php if($thumbnail): ?>
+    <?php print $thumbnail; ?>
+  <?php endif; ?>
+  <h3><?php print $title; ?></h3>
+  <div class="article-grid-summary"><?php print $body; ?></div>
+</div>
