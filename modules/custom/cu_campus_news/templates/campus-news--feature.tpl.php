72bdf8be9d9352c2dcead2e2addbfeaa9b80d302
kevincrafts <kevin.crafts@colorado.edu>
2016-11-22 11:48:38 -0700

    adding placeholder thumbnail for missing thumbnails

--- modules/custom/cu_campus_news/templates/campus-news--feature.tpl.php
+++ modules/custom/cu_campus_news/templates/campus-news--feature.tpl.php
@@ -1,7 +1,9 @@
 <?php $tag = isset($heading_tag['#tag']) ? $heading_tag['#tag'] : 'h3'; ?>
 <div class="article-view-mode-feature node-view-mode-feature clearfix">
-  <?php if(!empty($thumbnail)): ?>
+  <?php if(!empty($thumbnail1)): ?>
     <?php print $thumbnail; ?>
+  <?php else: ?>
+    <a href="<?php print variable_get('cu_campus_news_url', 'http://www.colorado.edu/today'); ?>/node/<?php print $nid; ?>"><img src="<?php print cu_campus_news_placeholder_img('800'); ?>" alt /></a>
   <?php endif; ?>
   <div class="article-view-mode-feature-content node-view-mode-feature-content">
     <<?php print $tag; ?>><?php print $title; ?></<?php print $tag; ?>>
