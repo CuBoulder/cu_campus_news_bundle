aa0b4111e0461fc6eaa01806da637c41958841a0
Kevin Crafts <kevin.crafts@colorado.edu>
2018-07-03 09:07:44 -0600

    fixing undefined variable issue

--- cu_campus_news/templates/campus-news--sidebar.tpl.php
+++ cu_campus_news/templates/campus-news--sidebar.tpl.php
@@ -1,5 +1,5 @@
 <div class="article-view-mode-sidebar node-view-mode-sidebar clearfix">
-  <?php if($thumbnail): ?>
+  <?php if(!empty($thumbnail)): ?>
     <?php print $thumbnail; ?>
   <?php endif; ?>
   <div class="article-view-mode-sidebar-content node-view-mode-sidebar-content">
